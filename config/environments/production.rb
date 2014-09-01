c24a60010a5b1bef860fa8f438f5ef4afb14f18e
prathamalag1994 <prathamalag@berkeley.edu>
2014-09-01 16:41:18 -0700

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,15 +63,23 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {:host => 'heroku.com'}
+  config.action_mailer.default_url_options = {:host => 'segamesapp.heroku.com'}
+  
+# ActionMailer Config
+# Setup for production - deliveries, no errors raised
 config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
-  :address => "127.0.0.1",
-  :port    => 25,
-  :domain  => 'heroku.com'
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "segamesapp.heroku.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["yoyopratham1994"],
+  password: ENV["fDakkudaddy1"]
 }
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
